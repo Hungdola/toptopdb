fd0291000fc04b0677cbc8161115291daa5d1726
Hungdola <92678824+Hungdola@users.noreply.github.com>
2022-09-29 04:26:19 +0700

    cau hinh router/layout cho du an

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { Fragment } from 'react'; //fragment chỉ là thẻ chứa thôi, nó không sinh ra element thật trong dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '@/routes';
+import { DefaultLayout } from '@/components/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+//layouts
+import { HeaderOnly } from '@/components/Layout/';
+
+//pages
+import Home from '@/pages/Home';
+import Following from '@/pages/Following';
+import Upload from '@/pages/Upload';
+import Profile from '@/pages/Profile';
+import Search from '@/pages/Search';
+
+//sẽ dùng cho những router mà bạn muốn ko cần đăng nhập vẫn xem được
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+//những router phải đăng nhập mới xem được
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
