90ed54ec46efbd68ceed7b3112abb21bb74a7507
Hungdola <92678824+Hungdola@users.noreply.github.com>
2022-09-30 02:36:17 +0700

    Dựng khung layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,16 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles); //bind sẽ giúp các bạn bind cái obj style này vào
+//rồi nó sẽ trả ra 1 method là 1  function class cx
+//rồi dùng này để dùng class khi đó nó giúp các bạn viết classname dưới dạng có dấu gạch ngang
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
