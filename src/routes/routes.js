9e1d9ff60ce33ec0c2bff723dab23dc25c7f84b6
Hungdola <92678824+Hungdola@users.noreply.github.com>
2022-10-25 02:52:52 +0700

    Xây dựng UI phần sidebar #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,11 +9,13 @@ import Following from '@/pages/Following';
 import Upload from '@/pages/Upload';
 import Profile from '@/pages/Profile';
 import Search from '@/pages/Search';
+import Live from '@/pages/Live';
 
 //sẽ dùng cho những router mà bạn muốn ko cần đăng nhập vẫn xem được
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
